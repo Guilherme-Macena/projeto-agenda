92a21d838e858544931f1601c9becb121d4fbf5d
Guilherme Macena <macenaguilherme1@gmail.com>
2021-10-20 22:09:42 -0300

    configurando models para criar tabelas no banco de dados.

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+    # máximo de caracteres para o campo
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(
+        max_length=255, blank=True)  # campo não obrigatório
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    # campo com uma data, importar pacotes.
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    # comparando uma tabela com a outra
+    categoia = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
