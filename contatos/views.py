a4f3c13ba8565c0e32978c47311792eca0d18d0f
Guilherme Macena <macenaguilherme1@gmail.com>
2021-10-22 21:52:51 -0300

    integrando models as views e iterando pela classe para exibir

--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Contato
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    # variavel que recebe todos os objetos da class Contato
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
