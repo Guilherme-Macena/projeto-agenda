a285c9adacb09121641b87018723e893b50449da
Guilherme Macena <macenaguilherme1@gmail.com>
2021-11-04 23:08:17 -0300

    atualizando

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 # Create your views here.
@@ -6,10 +7,14 @@ from django.core.paginator import Paginator
 
 def index(request):
     # variavel que recebe todos os objetos da class Contato
-    contatos = Contato.objects.all()
+    # ordenado do ultimo para o primeiro id adicionado
+    # filtrando pelo campo mostrar.
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
 
     # criando paginação
-    paginator = Paginator(contatos, 1)
+    paginator = Paginator(contatos, 2)
 
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -22,6 +27,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # variavel que recebe o id class Contato, com tratamento de erro 404
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:  # Validação antes de mostrar contatodetalhado
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
